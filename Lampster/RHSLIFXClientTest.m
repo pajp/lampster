8a121dc4ea3c8c6ec6f6cbe6dcfa90fd64eefb31
Rasmus Sten <rasmus@dll.nu>
2014-07-30 00:09:36 +0300

    Add a test for bulb hue (setting and getting)

--- Lampster/RHSLIFXClientTest.m
+++ Lampster/RHSLIFXClientTest.m
@@ -101,6 +101,42 @@
     return allLightsAtState;
 }
 
+- (void)testBulbHue
+{
+    [self invokeAndWaitForHandler:@selector(lightsOn:) onClient:self.client];
+    XCTAssert([self waitFor:^BOOL{
+        return [self allBulbsAtState:ON_STATE];
+    } seconds:5], @"Not all lights on");
+
+    for (CGFloat n = 0.0; n <= 7; n++) {
+        __block BOOL handlerCalled = NO;
+        CGFloat hue = 45.0 * n;
+        [self.client setColorHue:hue saturation:1.0 brightness:1.0 completionHandler:^(NSError *error) {
+            handlerCalled = YES;
+           // XCTAssertNotNil(error, @"There was an error setting color: %@", error);
+        }];
+        XCTAssertTrue([self waitFor:^BOOL{
+            return handlerCalled;
+        } seconds:10], @"Timeout waiting for setCololorHue… command to return");
+
+        BOOL success = [self waitFor:^BOOL{
+            BOOL bulbsAtTarget = YES;
+            NSArray* array = [self invokeAndWaitForArrayHandler:@selector(lightsStatus:) onClient:self.client];
+            XCTAssertTrue(array != nil, @"Lights-status return nil array");
+            XCTAssertTrue(array.count > 0, @"Lights-status array is empty");
+            for (NSDictionary* light in array) {
+                CGFloat currentHue = ((NSNumber*)light[@"hue"]).doubleValue;
+                if (round(currentHue) != hue) {
+                    bulbsAtTarget = NO;
+                    NSLog(@"Bulb %@ is not yet at target (current: %f, target: %f)", light[@"id"], currentHue, hue);
+                }
+            }
+            return bulbsAtTarget;
+        } seconds:10];
+        XCTAssert(success, @"All bulbs not at target hue (%f) after 10 seconds", hue);
+    }
+}
+
 - (void)testBulbsOn
 {
     [self invokeAndWaitForHandler:@selector(lightsOn:) onClient:self.client];
